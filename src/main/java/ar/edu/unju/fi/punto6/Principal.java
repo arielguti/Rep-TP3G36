3bf67304330c9b8339b692905be8948b9eb3e027
desc <desc@gato-pc>
2022-04-24 21:56:15 -0300

    metodo main completo

--- src/main/java/ar/edu/unju/fi/punto6/Principal.java
+++ src/main/java/ar/edu/unju/fi/punto6/Principal.java
@@ -1,5 +1,20 @@
 package ar.edu.unju.fi.punto6;
 
+import java.util.Scanner;
+
 public class Principal {
-//en un momento
+	public static void main(String[] args) {	
+		Scanner ingreso = new Scanner(System.in);
+		int[] arreglo= new int[10];
+		System.out.println("Ingrese 10 numeros para el nuevo arreglo");
+		for(int i=0;i<arreglo.length;i++) {
+			System.out.println("ingrese ingrese un numero entero para arreglo["+i+"]");
+			arreglo[i] = ingreso.nextInt();
+		}
+		System.out.println("numeros ingresados correctamente");
+		mostrarInversoDelArreglo(arreglo);
+	}
+	static void mostrarInversoDelArreglo(int [] numeros){
+		
+	}
 }
